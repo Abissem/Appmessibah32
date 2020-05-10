5e803b14037cbd5e316955a2f399f37fa047ee1b
Abissem <e95bso@live.fr>
2020-05-11 01:38:01 +0200

    [Feature][appelServeur] Ajout de l'appel serveur

---
+++ app/src/main/java/com/github/appmessibah32/Pokemon.java
@@ -0,0 +1,14 @@
+package com.github.appmessibah32;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
