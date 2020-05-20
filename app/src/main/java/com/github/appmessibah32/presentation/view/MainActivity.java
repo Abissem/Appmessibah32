a9220f35c328d3bd6e99f9db25ed3e1891c69bee
Abissem <e95bso@live.fr>
2020-05-21 00:34:05 +0200

    [Feature][architectureMVC] Add MVC Architecture for MainScreen

--- app/src/main/java/com/github/appmessibah32/presentation/view/MainActivity.java
+++ app/src/main/java/com/github/appmessibah32/presentation/view/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.github.appmessibah32.Constants;
 import com.github.appmessibah32.data.DragonApi;
 import com.github.appmessibah32.R;
+import com.github.appmessibah32.presentation.controller.MainController;
 import com.github.appmessibah32.presentation.model.Dragonball;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -28,7 +29,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final String BASE_URL = "https://dragon-ball-api.herokuapp.com/";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -36,37 +36,27 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private Gson gson;
 
+    private MainController controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("application_esiea", Context.MODE_PRIVATE);
-        gson = new GsonBuilder()
-                .setLenient()
-                .create();
+        controller = new MainController(
+                this,
+                 new GsonBuilder()
+                        .setLenient()
+                        .create(),
+                 getSharedPreferences("application_esiea", Context.MODE_PRIVATE)
 
-        List<Dragonball> characterList = getDataFromCache();
-
-        if(characterList != null){
-            showList(characterList);
-        } else {
-            makeApiCall();
-        }
+        );
+        controller.onStart();
     }
 
-    private List<Dragonball> getDataFromCache() {
-        String jsonCharacter = sharedPreferences.getString(Constants.KEY_CHARACTER_LIST, null);
 
-        if(jsonCharacter == null){
-            return null;
-        } else {
-            Type listType = new TypeToken<List<Dragonball>>(){}.getType();
-            return gson.fromJson(jsonCharacter, listType);
-        }
-    }
 
-    private void showList(List<Dragonball> characterList) {
+    public void showList(List<Dragonball> characterList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
         // improve performance if you know that changes
@@ -81,49 +71,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    private void makeApiCall(){
 
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        DragonApi dragonApi = retrofit.create(DragonApi.class);
-
-        Call<List<Dragonball>> call = dragonApi.getCharacterResponse();
-        call.enqueue(new Callback<List<Dragonball>>() {
-            @Override
-            public void onResponse(Call<List<Dragonball>> call, Response<List<Dragonball>> response) {
-                if(response.isSuccessful() && response.body() != null){
-                    List<Dragonball> characterList = response.body();
-                    saveList(characterList);
-                    showList(characterList);
-                } else {
-                    showError();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Dragonball>> call, Throwable t) {
-
-                showError();
-            }
-        });
-    }
-
-    private void saveList(List<Dragonball> characterList) {
-        String jsonString = gson.toJson(characterList);
-
-        sharedPreferences
-                .edit()
-                .putString(Constants.KEY_CHARACTER_LIST, jsonString)
-                .apply();
-        Toast.makeText(this, "List Saved", Toast.LENGTH_SHORT).show();
-
-    }
-
-    private void showError() {
+    public void showError() {
 
         Toast.makeText(this, "API Error", Toast.LENGTH_SHORT).show();
     }
