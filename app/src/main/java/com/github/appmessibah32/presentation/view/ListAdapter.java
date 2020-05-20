33bca202f0c348e802d62cf392d6c52631329c7c
Abissem <e95bso@live.fr>
2020-05-21 01:28:19 +0200

    [Feature][architectureMVC] Add listener for item click

--- app/src/main/java/com/github/appmessibah32/presentation/view/ListAdapter.java
+++ app/src/main/java/com/github/appmessibah32/presentation/view/ListAdapter.java
@@ -1,6 +1,5 @@
 package com.github.appmessibah32.presentation.view;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,18 +15,13 @@ import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     private List<Dragonball> values;
-    private Context context;
+    private OnItemClickListener listener;
 
-    public ListAdapter(Context context){
-        this.context=context;
+    public interface  OnItemClickListener {
+        void onItemClick(Dragonball item);
     }
 
-
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         TextView txtHeader;
         TextView txtFooter;
         TextView txtLast;
@@ -54,40 +48,40 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         notifyItemRemoved(position);
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public ListAdapter(List<Dragonball> myDataset) {
-        values = myDataset;
+    public ListAdapter(List<Dragonball> myDataset, OnItemClickListener listener) {
+        this.values = myDataset;
+        this.listener = listener;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public ListAdapter.ViewHolder onCreateViewHolder(
             ViewGroup parent,
             int viewType
     ) {
-        // create a new view
+        //On crée une nouvelle vue
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View v = inflater.inflate(R.layout.row_layout, parent, false);
-        // set the view's size, margins, paddings and layout parameters
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    //Remplace le contenu de la vue
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
 
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         final Dragonball currentCharacter = values.get(position);
 
         holder.txtHeader.setText(currentCharacter.getName());
         holder.txtFooter.setText("Origin planet : " + currentCharacter.getOriginPlanet());
         holder.txtLast.setText(currentCharacter.getGender());
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                listener.onItemClick(currentCharacter);
+            }
+        });
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return values.size();
